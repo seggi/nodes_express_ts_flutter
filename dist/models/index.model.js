55cddd68bfb9b3fab188aefdaab58e75f32ab43f
seggi <seggimarugira@gmail.com>
2021-08-01 19:59:57 -0400

    Add Authentication

--- dist/models/index.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-const sequelize_1 = require("sequelize");
-class User extends sequelize_1.Model {
-}
-exports.User = User;
-//# sourceMappingURL=index.model.js.map
--- dist/routes/admin.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const adminRouter = express_1.Router();
-adminRouter.get('/', (req, res) => {
-    return res.json("nk-auth-admin");
-});
-exports.default = adminRouter;
-//# sourceMappingURL=admin.route.js.map
