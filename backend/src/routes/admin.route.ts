942087f85f9519d285f07fc6a24f0af1b64431a9
seggi sms <seggimarugira@gmail.com>
2021-08-10 02:45:58 -0400

    Removing some console.log

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -7,20 +7,9 @@ import CheckLoggedIn from "../middlewares/loggedIn-user";
 const adminRouter: Router = Router();
 
 
-// export const getUserLogin: (() => RequestHandler) = (() => async (
-//     req: Request, res: Response, next: NextFunction
-// ) => {
-//     const token = getTokenFromHeaders(req.headers) || req.query.token || req.body.token || '';
-//     const hasAccess = UserController.getCurrentUser(token);
-//     console.log(hasAccess, "LLL")
-//     return next()
-// })
-
 
  adminRouter.get('/loggin_user', async (req, res) => {
-    console.log(req.headers.authorization, "KKK")
     const getUser = await CheckLoggedIn.getUser(req)
-    console.log( getUser, "HHHH");
     return res.json({ data: getUser })
 })
 
